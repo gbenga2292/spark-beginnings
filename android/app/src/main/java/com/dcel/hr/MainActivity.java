948c00478dc52bbf9a8c9b08eaffadd29bd6f3c4
Gbenga2292 <jbk24014@gmail.com>
2026-04-12 11:46:47 +0100

    andriod 1

---
+++ android/app/src/main/java/com/dcel/hr/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dcel.hr;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dcel.hr',
+  appName: 'DCEL Office Suite',
+  webDir: 'dist'
+};
+
+export default config;
